cc2fa57b1edf2bc6a35a4c21889b1b174f961417
Abdelrahman-Hesham-2000 <abdohesham281@gmail.com>
2024-10-06 17:10:15 +0300

    Task_1 is done

---
+++ materials/python_for_ML/tasks/Task_1.py
@@ -0,0 +1,17 @@
+address='Amit_ml@gmail.edu'
+if address.count('@') != 1:
+    print("Invalid email")
+else:
+    username, domain = address.split('@')
+    if address.count('.') == 0:
+        print("Invalid email")
+    else:
+        print("Valid email")
+print(username)
+print(domain)
+if address.endswith(".com"):
+    print("Commercial")
+elif address.endswith(".edu"):
+    print("Educational")
+else:
+    print("Other Domain")
