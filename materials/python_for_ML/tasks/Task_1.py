a9a3e4142ef57927ae01205bcca53065a0c1988e
George Samuel Mahrous <31563545+gsamueil@users.noreply.github.com>
2024-10-13 12:42:00 +0300

    Update Task_1.py

--- materials/python_for_ML/tasks/Task_1.py
+++ materials/python_for_ML/tasks/Task_1.py
@@ -9,6 +9,25 @@ else:
         print("Valid email")
 print(username)
 print(domain)
+#here you can use another way ..... 
+''' 
+domain_types = [
+        (".com", "Commercial"),
+        (".edu", "Educational"),
+        (".org", "Organization"),
+        (".gov", "Government")
+    ]
+
+    # Loop through the list of domain types and check if address ends with any of them
+    for domain, domain_name in domain_types:
+        if address.endswith(domain):
+            print(f"This is a {domain_name} domain")
+            return  # Exit once the domain type is found
+
+    # If none of the above match, print "Other Domain"
+    print("Other Domain")
+'''
+
 if address.endswith(".com"):
     print("Commercial")
 elif address.endswith(".edu"):
